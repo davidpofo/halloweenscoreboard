7914a1666a3d5f2b31d145418579ee4b4ae9c2cb
davidpofo <dampofo@umd.edu>
2019-10-24 00:25:52 -0400

    removed yarn lock and added package json

--- src/components/App.js
+++ src/components/App.js
@@ -33,11 +33,13 @@ class App extends Component {
   //player id counter
   prevPlayerId = 4;
 
-  getHighScore = () => {
+  getSortedScores = () => {
   const scores = this.state.players.map( p => p.score );
   const highScore = Math.max(...scores);
-  if (highScore) {
-    return highScore;
+  var sortedscores = scores.sort((a,b) => a.timeM - b.timeM);
+
+  if (sortedscores) {
+    return sortedscores;
   }
   return null;
 };
@@ -71,7 +73,15 @@ class App extends Component {
   }
 
   render() {
-    const highScore = this.getHighScore();
+    const sortedscores = this.getSortedScores();
+    const topthree = sortedscores.slice(0, 3).map(i => {
+      return <myview item={i} key={i.id} />
+  }
+
+    const highScore = topthree.slice(0, 2);
+    const secondScore = topthree.slice(1, 2);
+    const thirdScore = topthree.slice(2, 3);;
+
 
     return (
       <div className="scoreboard">
