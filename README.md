7be11756353452c2f85b5b0edfd678e6156cd64d
davidpofo <dampofo@umd.edu>
2019-10-25 09:52:11 -0400

    Revert "removed yarn lock and added package json"
    Added second and third crown logic and css coloring

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Halloween Scoreboard
-- A react app with an express server.
+- A Halloween Scoreboard for your spooky holiday parties built with React and NodeJS.
 ## Feature highlight 
 1) Default of four Players with the option to add or remove players.
 2) The top three players have different crown colors (Gold- 1st, Silver - 2nd, Bronze 3rd)
--- src/components/App.js
+++ src/components/App.js
@@ -8,22 +8,22 @@ class App extends Component {
   state = {
     players: [
       {
-        name: "Team 1",
+        name: "Player 1",
         score: 0,
         id: 1
       },
       {
-        name: "Team 2",
+        name: "Player 2",
         score: 0,
         id: 2
       },
       {
-        name: "Team 3",
+        name: "Player 3",
         score: 0,
         id: 3
       },
       {
-        name: "Team 4",
+        name: "Player 4",
         score: 0,
         id: 4
       },
